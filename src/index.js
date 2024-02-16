42e4f36aa8ef543b8ee525d0063665f22b2bcec3
MMolik <molikos@o2.pl>
2024-02-16 18:36:31 +0100

    site working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-import axios from "axios";
+import { fetchBreeds } from './cat-api';
+import { fetchCatByBreed } from './cat-api';
 
-axios.defaults.headers.common["live_REuLXp4pSTQtbXA6cfFumqL6m7PAi7VDnyxVcq4AcbGkBbfxUEbcwzITRBdVUESx"]
+
+const breedSelect = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+
+breedSelect.addEventListener('click', () => {
+  try {
+    fetchBreeds().then(data => renderSelect(data));
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+function renderSelect(breeds) {
+  const markup = breeds
+    .map(({ id, name }) => {
+      return `<option value="${id}">${name}</option>`;
+    })
+    .join('');
+  breedSelect.insertAdjacentHTML('beforeend', markup);
+}
+
+breedSelect.addEventListener('change', e => {
+  fetchCatByBreed(e.target.value).then(data => renderCat(data[0]));
+});
+
+function renderCat(catData) {
+  const { url } = catData;
+  const { description, name, temperament } = catData.breeds[0];
+  catInfo.insertAdjacentHTML(
+    'beforeend',
+    `<div>
+          <h2>${name}</h2>
+          <img src="${url}" alt="${name}" />
+          <p>${description}</p>
+          <p><strong>Temperament:</strong> ${temperament}</p>
+      </div>`
+  );
+}
